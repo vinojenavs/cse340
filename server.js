aa1f94894118c5e6c5a2eeec68627955029cdd13
vinojenavs <oajenavi@bupathway.edu>
2026-07-01 19:01:09 +0100

    Initial Site Creation

--- server.js
+++ server.js
@@ -32,9 +32,13 @@ app.get('/organizations', async (req, res) => {
     const title = 'Our Partner Organizations'
     res.render('organizations', { title });
 });
-app.get('/projects', (req, res) => {
+app.get('/projects', async (req, res) => {
     const title = 'Service Projects'
     res.render('projects', { title });
+});
+app.get('/categories', async (req, res) => {
+    const title = 'Categories'
+    res.render('categories', { title });
 });
 
 
