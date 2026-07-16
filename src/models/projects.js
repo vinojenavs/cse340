33a4d19645c670b1dda68e91bd2f27a76ba309b8
vinojenavs <oajenavi@bupathway.edu>
2026-07-16 16:42:04 +0100

    organization details page

--- src/models/projects.js
+++ src/models/projects.js
@@ -7,6 +7,26 @@ const getAllProjectServices = async () => {
     ON org.organization_id = pro.organization_id;`
     const result = await db.query(query);
     return result.rows;
-}
+};
 
-export { getAllProjectServices }
+const getProjectsByOrganizationId = async (organizationId) => {
+      const query = `
+        SELECT
+          project_id,
+          organization_id,
+          title,
+          description,
+          location,
+          project_date
+        FROM projects
+        WHERE organization_id = $1
+        ORDER BY project_date;
+      `;
+      
+      const queryParams = [organizationId];
+      const result = await db.query(query, queryParams);
+
+      return result.rows;
+};
+
+export { getAllProjectServices, getProjectsByOrganizationId }
--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ import { organizationsPage } from './controllers/organizations.js';
 import { projectsPage } from './controllers/projects.js';
 import { categoriesPage } from './controllers/categories.js';
 import { errorPage } from './controllers/error.js';
+import { organizationDetailPage } from './controllers/organizations.js';
 
 
 const router = express.Router();
@@ -12,6 +13,7 @@ router.get('/', homePage);
 router.get('/organizations', organizationsPage);
 router.get('/projects', projectsPage);
 router.get('/categories', categoriesPage);
+router.get('/organization/:id', organizationDetailPage);
 
 // error-handling routes
 router.get('/test-error', errorPage);
